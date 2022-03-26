8c81c22214c80e968e0ac75f47ec01cc404b6026
wackod <36387604+wackod@users.noreply.github.com>
2022-03-27 04:27:54 +0800

    FIX

--- addons/sourcemod/scripting/zescape/voice_chat.h
+++ addons/sourcemod/scripting/zescape/voice_chat.h
@@ -15,18 +15,22 @@ void VoiceChatOnClientConnected(int client)
 {
 
 }*/
+
 Action VoiceChatSettingCommand(int client,int args)
 {
-	if(GetClientListeningFlags(client) && VOICE_TEAM)
-	{
-	PrintToChat(client,"[语音频道]现在你开麦说话所有人都可以听见.");
-	SetClientListeningFlags(client,VOICE_SPEAKALL);
-	}
-	if(GetClientListeningFlags(client) && VOICE_SPEAKALL)
-	{
-	PrintToChat(client,"[语音频道]现在你开麦说话只有队友可以听见.");
-	SetClientListeningFlags(client,VOICE_TEAM);
-	}
 	
+	if(GetClientListeningFlags(client) & VOICE_TEAM)
+	{
+	PrintToChat(client,"[语音频道] 现在你开麦说话所有人都可以听见.");
+	SetClientListeningFlags(client,VOICE_SPEAKALL);
 	return Plugin_Handled;
+	}
+	if(GetClientListeningFlags(client) & VOICE_SPEAKALL)
+	{
+	PrintToChat(client,"[语音频道] 现在你开麦说话只有队友可以听见.");
+	SetClientListeningFlags(client,VOICE_TEAM);
+	return Plugin_Handled;
+	}
+	return Plugin_Continue;
 }
+
