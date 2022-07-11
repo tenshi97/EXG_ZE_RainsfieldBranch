76ced067c9e9d0a1d14a860dca26786224c8a2c5
nullable <33@331337.xyz>
2022-07-11 11:24:45 +0800

    Update trigger_output.h

--- addons/sourcemod/scripting/zescape/trigger_output.h
+++ addons/sourcemod/scripting/zescape/trigger_output.h
@@ -1,5 +1,6 @@
-bool Button_Spam_Client[65];
+// bool Button_Spam_Client[65];
 //bool Trigger_Spam_Client[65];
+/*
 void TriggerOutputOnPluginStart()
 {
 	for(int i=1;i<=64;i++)
@@ -7,7 +8,8 @@ void TriggerOutputOnPluginStart()
 		Button_Spam_Client[i]=false;
 //		Trigger_Spam_Client[i]=false;
 	}
-	HookEntityOutput("func_button", "OnPressed", ButtonOnPressed);
+	// HookEntityOutput("func_button", "OnPressed", ButtonOnPressed);
+	// HookEntityOutput("func_button", "OnDamaged", ButtonOnPressed);
 //	HookEntityOutput("trigger_once", "OnStartTouch", TriggerOnTouch);
 //	HookEntityOutput("trigger_multiple", "OnStartTouch", TriggerOnTouch);
 //	HookEntityOutput("trigger_once", "OnEndTouch", TriggerOnTouch);
@@ -21,25 +23,33 @@ void TriggerOutputOnMapStart()
 //		Trigger_Spam_Client[i]=false;
 	}	
 }
-void ButtonOnPressed(const char[] output, int caller, int activator, float delay)
+*/
+
+public Action ButtonOnPressed(const char[] output, int caller, int activator, float delay)
 {
 	if (GetEntPropEnt(caller, Prop_Data, "m_hMoveParent") != -1 ||
 		GetEntPropEnt(caller, Prop_Data, "m_hMoveChild") != -1 ||
-		GetEntPropEnt(caller, Prop_Data, "m_hMovePeer") != -1) return;	
-	if(activator<1||activator>64)	return;
-	if(!IsClientInGame(activator))	return;
-	if(!Button_Spam_Client[activator])
-	{
-		Button_Spam_Client[activator] = true;
-	}
-	else
-	{
-		return;
-	}
-	CreateTimer(5.0, ButtonOnPressedCallback, activator, TIMER_FLAG_NO_MAPCHANGE);
+		GetEntPropEnt(caller, Prop_Data, "m_hMovePeer") != -1)
+		return Plugin_Continue;
+
+	if(activator<1 || activator>64)
+		return Plugin_Continue;
+	
+	if(!IsClientInGame(activator))
+		return Plugin_Continue;
+	
+	// if(!Button_Spam_Client[activator])
+	// {
+	// 	Button_Spam_Client[activator] = true;
+	// }
+	// else
+	// {
+	// 	return;
+	// }
+	// CreateTimer(5.0, ButtonOnPressedCallback, activator, TIMER_FLAG_NO_MAPCHANGE);
 
 	char activator_name[64];
-	GetClientName(activator,activator_name,sizeof(activator_name));
+	GetClientName(activator, activator_name, sizeof(activator_name));
 
 	char activator_auth[64];
 	GetClientAuthId(activator, AuthId_Steam2, activator_auth, sizeof(activator_auth), true);
@@ -48,31 +58,52 @@ void ButtonOnPressed(const char[] output, int caller, int activator, float delay
 	char button_name[64];
 	GetEntPropString(caller, Prop_Data, "m_iName", button_name, sizeof(button_name));
 	
-	PrintToChatAll(" \x04[触发检测] \x09%s(%s) \x01触发了 \x06按钮%s", activator_name, activator_auth, button_name);
-	PrintToConsoleAll("[触发检测] %s(%s) 触发了 按钮(Button)%s", activator_name, activator_auth, button_name);
+	int hammerid = GetEntProp(caller, Prop_Data, "m_iHammerID");
+
+	PrintToChatAll(" \x04[触发检测] \x09%s(%s) \x01触发了 \x06按钮%s(%d)", activator_name, activator_auth, button_name, hammerid);
+	PrintToConsoleAll("[触发检测] %s(%s) 触发了 按钮(Button)%s(%d)", activator_name, activator_auth, button_name, hammerid);
+
+	return Plugin_Continue;
 }
 
-Action ButtonOnPressedCallback(Handle timer, int activator) {
-	if (!Button_Spam_Client[activator]) return Plugin_Handled;
-	Button_Spam_Client[activator] = false;
-	return Plugin_Handled;
-}
+// Action ButtonOnPressedCallback(Handle timer, int activator) {
+// 	if (!Button_Spam_Client[activator]) return Plugin_Handled;
+// 	Button_Spam_Client[activator] = false;
+// 	return Plugin_Handled;
+// }
 
 void TriggerOnEntityCreated(int entity, const char[] classname)
 {
-	if (strcmp(classname, "trigger_once", false) != 0 && strcmp(classname, "trigger_multiple", false) != 0)
-		return;
-
 	if (IsValidEntity(entity))
 	{
-		SDKHook(entity, SDKHook_SpawnPost, TriggerOnEntitySpawnPost);
+		if (strcmp(classname, "trigger_once", false) == 0 || strcmp(classname, "trigger_multiple", false) != 0)
+		{
+			SDKHook(entity, SDKHook_SpawnPost, TriggerOnTriggerSpawnPost);
+		}
+		else if (strcmp(classname, "func_button", false) == 0)
+		{
+			SDKHook(entity, SDKHook_SpawnPost, TriggerOnButtonSpawnPost);
+		}
 	}
 }
 
-public void TriggerOnEntitySpawnPost(int entity)
+public void TriggerOnButtonSpawnPost(int entity)
 {
 	if (!IsValidEntity(entity))
 		return;
+
+	SDKUnhook(entity, SDKHook_SpawnPost, TriggerOnButtonSpawnPost);
+
+	HookSingleEntityOutput(entity, "func_button", "OnPressed", ButtonOnPressed, true);
+	HookSingleEntityOutput(entity, "func_button", "OnDamaged", ButtonOnPressed, true);
+}
+
+public void TriggerOnTriggerSpawnPost(int entity)
+{
+	if (!IsValidEntity(entity))
+		return;
+
+	SDKUnhook(entity, SDKHook_SpawnPost, TriggerOnTriggerSpawnPost);
 
 	SDKHook(entity, SDKHook_StartTouch, TriggerOnStartTouch);
 }
@@ -80,21 +111,30 @@ public void TriggerOnEntitySpawnPost(int entity)
 public Action TriggerOnStartTouch(int entity, int toucher)
 {
 	SDKUnhook(entity, SDKHook_StartTouch, TriggerOnStartTouch);
+
 	if (IsClientInGame(toucher))
 	{
-	char activator_name[64];
-	GetClientName(toucher, activator_name, sizeof(activator_name));
+		char activator_name[64];
+		GetClientName(toucher, activator_name, sizeof(activator_name));
 
-	char activator_auth[64];
-	GetClientAuthId(toucher, AuthId_Steam2, activator_auth, sizeof(activator_auth), true);
-	ReplaceString(activator_auth, sizeof(activator_auth), "STEAM_1:", "", false);
+		char activator_auth[64];
+		GetClientAuthId(toucher, AuthId_Steam2, activator_auth, sizeof(activator_auth), true);
+		ReplaceString(activator_auth, sizeof(activator_auth), "STEAM_1:", "", false);
 
-	char button_name[64];
-	GetEntPropString(entity, Prop_Data, "m_iName", button_name, sizeof(button_name));
-	
-	PrintToChatAll(" \x04[触发检测] \x09%s(%s) \x01触发了 \x06机关%s", activator_name, activator_auth, button_name);
-	PrintToConsoleAll("[触发检测] %s(%s) 触发了 机关(Trigger)%s", activator_name, activator_auth, button_name);
-}
+		char button_name[64];
+		GetEntPropString(entity, Prop_Data, "m_iName", button_name, sizeof(button_name));
+
+		if (button_name[0] == NULL_STRING[0])
+		{
+			GetEntPropString(entity, Prop_Data, "m_iGlobalname", button_name, sizeof(button_name));
+		}
+		
+		int hammerid = GetEntProp(entity, Prop_Data, "m_iHammerID");
+
+		PrintToChatAll(" \x04[触发检测] \x09%s(%s) \x01触发了 \x06机关%s(%d)", activator_name, activator_auth, button_name, hammerid);
+		PrintToConsoleAll("[触发检测] %s(%s) 触发了 机关(Trigger)%s(%d)", activator_name, activator_auth, button_name, hammerid);
+	}
+
 	return Plugin_Continue;
 }
 
